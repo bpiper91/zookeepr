c3617be38964e8a6dd4e46eba680f6f199547fa5
bpiper91 <bpiper91@gmail.com>
2022-08-10 22:42:16 -0400

    added routes for page requests for /animals, /zookeepers, and /*

--- server.js
+++ server.js
@@ -104,17 +104,29 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
-  
+
     // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
+        res.status(400).send('The animal is not properly formatted.');
     } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal);
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
 });
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
